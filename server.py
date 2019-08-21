f143ed2546f02dca455eb5761c99ee52e2204273
Kuczora Tibor <kuczora.tibi@gmail.com>
2019-08-21 21:06:05 +0200

    Add new function: generate_id Update function route_add_edit with new handler

--- server.py
+++ server.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 QUESTION_TABLE = 'sample_data/question.csv'
 
 
+def generate_id():
+    return len(data_handler.get_data(QUESTION_TABLE))+1
+
+
 @app.route("/")
 @app.route("/list")
 def route_list():
@@ -17,13 +21,22 @@ def route_list():
 @app.route("/add_edit/<int:id>", methods=["POST", "GET"])
 def route_add_edit(id_=None):
 
-    if request.method == "GET" and id_ is not None:  # When someone clicks on a title
+    if request.method == "GET" and id_ is not None:  # When you click on a title
         table = data_handler.get_data(QUESTION_TABLE)
         row = data_handler.get_row_by_id(table, id_)
         row_id = row[0]
         row_title = row[1]
         row_message = row[2]
-        return render_template("add_edit.html", row_id=row_id, row_title=row_title, row_message=row_message, title="Edit question")
+        return render_template("add_edit.html", id_=row_id, row_title=row_title, row_message=row_message, title="Edit question")
+
+    if request.method == "POST" and id_ is None: # When you finished adding a new question
+        row = [
+            generate_id(),
+            request.form["title"],
+            request.form["message"]
+        ]
+        data_handler.append_table(QUESTION_TABLE, row)
+        return redirect("/")
 
     return render_template("add_edit.html", id_=None, title='Add new question')  # When someone clicks on add new question button
 
