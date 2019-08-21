91a1a7022d96de38c1147b0abc497d1bfe31ef02
Kuczora Tibor <kuczora.tibi@gmail.com>
2019-08-21 14:14:32 +0200

    Add router_add_edit function with route

--- server.py
+++ server.py
@@ -12,5 +12,14 @@ def route_list():
     return render_template("list.html", question_table=question_table)
 
 
+@app.route("/add_edit", methods=["POST", "GET"])
+@app.route("/add_edit/<int:id>", methods=["POST", "GET"])
+def route_add_edit(id=None):
+
+    if request.method == "GET" and id is not None:  # When someone clicks on a title
+        row = get_row_by_id(get_table(TABLE), id)
+        return render_template("add_edit.html", row=row)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
