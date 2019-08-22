c6042861ab1c0fb039b567ce6ecc375402de9d7f
Kuczora Tibor <kuczora.tibi@gmail.com>
2019-08-22 11:07:07 +0200

    Big fking commit

--- server.py
+++ server.py
@@ -13,21 +13,11 @@ def route_list():
     return render_template("list.html", question_table=question_table)
 
 
-@app.route("/add_edit", methods=["POST", "GET"])
-@app.route("/add_edit/<id_>", methods=["POST", "GET"])
-def route_add_edit(id_=None):
-
-    # When you click on an ID
-    if request.method == "GET" and id_ is not None:
-        table = data_handler.get_data(QUESTION_TABLE)
-        row = data_handler.get_row_by_id(table, id_)
-        row_id = row[0]
-        row_title = row[1]
-        row_message = row[2]
-        return render_template("add_edit.html", id_=row_id, row_title=row_title, row_message=row_message, title="Edit question")
+@app.route("/add", methods=["POST", "GET"])
+def route_add():
 
     # When you finished adding a new question
-    if request.method == "POST" and id_ is None:
+    if request.method == "POST":
         row = [
             data_handler.generate_id(data_handler.get_data(QUESTION_TABLE)),
             request.form["title"],
@@ -36,6 +26,14 @@ def route_add_edit(id_=None):
         data_handler.add_element(QUESTION_TABLE, row)
         return redirect("/")
 
+    # When someone clicks on add new question button
+    return render_template("add_edit.html", id_=None, title='Add new question')
+
+
+@app.route("/edit", methods=["POST", "GET"])
+@app.route("/edit/<id_>", methods=["POST", "GET"])
+def route_edit(id_=None):
+
     # When you finished updating the question
     if request.method == "POST" and id_ is not None:
         row = [
@@ -43,11 +41,19 @@ def route_add_edit(id_=None):
             request.form["title"],
             request.form["message"]
         ]
-        data_handler.update_row_in_table_by_id(QUESTION_TABLE, row, id_)
+        new_question_table = data_handler.edit_element(row, QUESTION_TABLE)
+        data_handler.save_data(QUESTION_TABLE, new_question_table)
         return redirect("/")
 
-    # When someone clicks on add new question button
-    return render_template("add_edit.html", id_=None, title='Add new question')
+    # When you click on an ID
+    if id_ is not None:
+        table = data_handler.get_data(QUESTION_TABLE)
+        row = data_handler.get_row_by_id(table, id_)
+        row_id = row[0]
+        row_title = row[1]
+        row_message = row[2]
+        return render_template("add_edit.html", id_=row_id, row_title=row_title, row_message=row_message,
+                               title="Edit question")
 
 
 @app.route("/question_details/<id_>")
