66c9cc2a7f1aea3e99ab26ce6127495964aa3076
Anna Czinege <anna.czinege3@gmail.com>
2019-09-02 14:05:44 +0200

    Add add_new_question function to sql_handler.py

--- sql_handler.py
+++ sql_handler.py
@@ -5,7 +5,22 @@ import database_common
 def list_all_questions(cursor):
     cursor.execute("""
                     SELECT id, title
-                    FROM questions
+                    FROM questions;
                     """)
     question_table = cursor.fetchall()
     return question_table
+
+
+@database_common.connection_handler
+def add_new_question(cursor, add_dict):
+    title = add_dict['title']
+    message = add_dict['message']
+    cursor.execute("""
+                    INSERT INTO questions
+                    VALUES (%(title)s, %(message)s);
+                    """,
+                   {'title': title, 'message': message})
+
+
+@database_common.connection_handler
+de
