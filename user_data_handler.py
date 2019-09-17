03ef1be6df5dbf5fb45f9041b82668fe47146d40
Kuczora Tibor <kuczora.tibi@gmail.com>
2019-09-17 13:28:36 +0200

    Add get_username_by_id query to user_data_handler.py

--- user_data_handler.py
+++ user_data_handler.py
@@ -37,3 +37,11 @@ def get_userid_by_username(cursor, username):
                     WHERE username = '{username}'
                     """).format(username=sql.SQL(username)))
     return sql_handler.normalize_output_single_row(cursor.fetchall())
+
+@database_common.connection_handler
+def get_username_by_id(cursor, userid):
+    cursor.execute(sql.SQL("""
+                    SELECT username FROM users
+                    WHERE id = '{userid}'
+                    """).format(username=sql.SQL(userid)))
+    return sql_handler.normalize_output_single_row(cursor.fetchall())
