0805ffb61a24e52cd39150c523b18a9d685503b0
rasgacode <rongybaba12@gmail.com>
2019-09-19 19:25:30 +0200

    Fix accepted answer button

--- static/js/error_handling.js
+++ static/js/error_handling.js
@@ -1,11 +1,6 @@
-let logMessage = document.querySelector(".log-message");
-
 let errorHandler = function () {
+    let logMessage = document.querySelector(".log-message");
     if (logMessage.innerText === 'You are not logged in') {
         alert(logMessage.innerText);
     }
-};
-
-let loginHandler = function () {
-
 };
